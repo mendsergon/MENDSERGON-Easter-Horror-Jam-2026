562dae13a0a89550276a3de7181358ba931558d7
mendsergon <skaloumenos70@protonmail.com>
2026-03-10 00:06:42 +0200

    Added player sprite, improved player code, added flashlight sprite and hold logic

--- easter-horror-jam/scripts/player/player.gd
+++ easter-horror-jam/scripts/player/player.gd
@@ -1,26 +1,40 @@
 extends CharacterBody2D
 
-@export var speed: float = 300.0
-@export var acceleration: float = 1500.0
-@export var friction: float = 1200.0
+@export var speed: float = 150.0
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var flashlight: Node2D = $Flashlight
 
 var last_direction: Vector2 = Vector2.DOWN
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
+	# Get input 
 	var input_direction := Vector2.ZERO
 	input_direction.x = Input.get_axis("ui_left", "ui_right")
 	input_direction.y = Input.get_axis("ui_up", "ui_down")
 	
+	# If moving, update direction and velocity
 	if input_direction != Vector2.ZERO:
 		input_direction = input_direction.normalized()
 		last_direction = input_direction
-	
-	if input_direction != Vector2.ZERO:
-		velocity = velocity.move_toward(input_direction * speed, acceleration * delta)
+		velocity = input_direction * speed
+		animated_sprite_2d.play("walk")
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+		# Stop moving and play idle animation
+		velocity = Vector2.ZERO
+		animated_sprite_2d.play("idle")
+	
+	# Flip sprite based on horizontal direction
+	if last_direction.x < 0:
+		animated_sprite_2d.flip_h = true
+		flashlight.position.x = -8
+		flashlight.scale.x = -1
+	elif last_direction.x > 0:
+		animated_sprite_2d.flip_h = false
+		flashlight.position.x = 8
+		flashlight.scale.x = 1
 	
 	move_and_slide()
 
+# Get the direction the player is facing
 func get_facing_direction() -> Vector2:
 	return last_direction
